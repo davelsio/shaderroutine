aee480f4e7321f417cb5de1158d5dd6809129168
David Velasco <davvelsan@gmail.com>
2025-05-22 01:17:35 +0200

    simplify useImages hook logic

--- hooks/useImages.ts
+++ hooks/useImages.ts
@@ -1,37 +1,19 @@
 import { Skia, type SkImage } from '@shopify/react-native-skia';
 import { Asset } from 'expo-asset';
-import { useCallback, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import { withAbort } from '@utils/withAbort';
 
+const imageCache = new Map<string, SkImage>();
+
+/**
+ * Load a set of Skia images using their URIs.
+ * @param imageURIs image URIs
+ */
 export function useImages(imageURIs: string[]) {
   const [loading, setLoading] = useState<boolean>(false);
   const [error, setError] = useState<string | null>(null);
   const [images, setImages] = useState<SkImage[]>([]);
-
-  /**
-   * Load images from a set of URIs.
-   */
-  const loadImages = useCallback(async (uris: string[]) => {
-    const skImages = uris.map(async (uri) => {
-      const asset = await Asset.fromURI(uri).downloadAsync();
-
-      if (!asset.localUri) {
-        throw new Error('Failed to download image');
-      }
-
-      const data = await Skia.Data.fromURI(asset.localUri);
-      const skImage = Skia.Image.MakeImageFromEncoded(data);
-
-      if (!skImage) {
-        throw new Error('Failed to create image');
-      }
-
-      return skImage;
-    });
-
-    return Promise.all(skImages);
-  }, []);
 
   /**
    * Clean up unused Skia resources.
@@ -61,7 +43,38 @@ export function useImages(imageURIs: string[]) {
     return () => {
       abortController.abort();
     };
-  }, [imageURIs, loadImages]);
+  }, [imageURIs]);
 
   return { error, loading, images };
+}
+
+/**
+ * Load the Skia images from remote or cache.
+ * @param uris image URIs
+ */
+function loadImages(uris: string[]) {
+  const skImages = uris.map(async (uri) => {
+    if (imageCache.has(uri)) {
+      return imageCache.get(uri)!;
+    }
+
+    const asset = await Asset.fromURI(uri).downloadAsync();
+
+    if (!asset.localUri) {
+      throw new Error('Failed to download image');
+    }
+
+    const data = await Skia.Data.fromURI(asset.localUri);
+    const skImage = Skia.Image.MakeImageFromEncoded(data);
+
+    if (!skImage) {
+      throw new Error('Failed to create image');
+    }
+
+    imageCache.set(uri, skImage);
+
+    return skImage;
+  });
+
+  return Promise.all(skImages);
 }
