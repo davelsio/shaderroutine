775a8c26aeedd1a95c7384a3193eed41580fcd33
David Velasco <davvelsan@gmail.com>
2025-07-19 11:45:05 +0200

    feat: CircularSlider component

---
+++ src/components/CircularSlider/CircularSliderItem.tsx
@@ -0,0 +1,50 @@
+import { useMemo } from 'react';
+import { Image } from 'react-native';
+import Animated, {
+  interpolate,
+  interpolateColor,
+  type SharedValue,
+  useAnimatedStyle,
+} from 'react-native-reanimated';
+
+import styles, { ITEM_SIZE } from './CircularSlider.styles';
+
+type CarouselItemProps = {
+  imageUri: string;
+  index: number;
+  scrollX: SharedValue<number>;
+};
+
+export function CircularSliderItem({
+  imageUri,
+  index,
+  scrollX,
+}: CarouselItemProps) {
+  const source = useMemo(() => ({ uri: imageUri }), [imageUri]);
+
+  const animatedStyle = useAnimatedStyle(() => {
+    const translateDown = ITEM_SIZE / 3;
+    return {
+      borderColor: interpolateColor(
+        scrollX.value,
+        [index - 1, index, index + 1],
+        ['transparent', 'white', 'transparent']
+      ),
+      transform: [
+        {
+          translateY: interpolate(
+            scrollX.value,
+            [index - 1, index, index + 1],
+            [translateDown, 0, translateDown]
+          ),
+        },
+      ],
+    };
+  });
+
+  return (
+    <Animated.View style={[styles.carouselItem, animatedStyle]}>
+      <Image source={source} style={styles.carouselImage} />
+    </Animated.View>
+  );
+}
