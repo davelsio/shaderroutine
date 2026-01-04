e5ce14e8c25b345029c47b2f1459dc333ae5111f
David Velasco <7377316+davelsio@users.noreply.github.com>
2026-01-04 09:55:48 +0100

    refactor: remove metro config defaults and unused config

--- metro.config.js
+++ metro.config.js
@@ -1,26 +1,28 @@
 /* eslint-env node */
 
-// Learn more https://docs.expo.dev/guides/monorepos
-// Learn more https://docs.expo.io/guides/customizing-metro
+/**
+ * Relevant Expo documentation.
+ * @see https://docs.expo.io/guides/customizing-metro
+ * @see https://docs.expo.dev/guides/monorepos
+ * @see https://docs.expo.dev/versions/latest/config/metro/#es-module-resolution
+ * @see https://docs.swmansion.com/react-native-reanimated/docs/debugging/accurate-call-stacks/
+ */
 
 const { getDefaultConfig } = require('expo/metro-config');
 const {
   wrapWithReanimatedMetroConfig,
 } = require('react-native-reanimated/metro-config');
 
-// Find the project and workspace directories
-const projectRoot = __dirname;
-
 /** @type {import('expo/metro-config').MetroConfig} */
-const config = getDefaultConfig(projectRoot, {
-  // [Web-only]: Enables CSS support in Metro.
-  isCSSEnabled: true,
+const config = getDefaultConfig(__dirname, {
+  // Custom config
 });
 
+// Support shader extensions
 config.resolver.assetExts.push('glsl', 'sksl');
 
 /**
- * Opt-out of the new ES Modules resolution.
+ * Uncomment to opt-out of the new ES Modules resolution.
  * @see https://docs.expo.dev/versions/latest/config/metro/#es-module-resolution
  */
 // config.resolver.unstable_enablePackageExports = false;
