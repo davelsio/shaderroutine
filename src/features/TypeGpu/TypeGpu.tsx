9a0c858d02a876a94c268c5fe46001389efaa3d1
David Velasco <7377316+davelsio@users.noreply.github.com>
2026-01-17 11:24:28 +0100

    feat: useTypeGPU hook

--- src/features/TypeGpu/TypeGpu.tsx
+++ src/features/TypeGpu/TypeGpu.tsx
@@ -2,73 +2,40 @@
  * Adapted from https://docs.swmansion.com/TypeGPU/examples/#example=rendering--caustics
  */
 
-import { useClock } from '@shopify/react-native-skia';
-import { Canvas, useDevice, type CanvasRef } from 'react-native-wgpu';
-import { tgpu } from 'typegpu';
+import { Canvas } from 'react-native-wgpu';
+
+import { useTypeGPU } from '@hooks/useTypeGPU';
 
 import { createFragmentShader } from './TypeGpu.frag';
 import style from './TypeGpu.styles';
 import { mainVertex } from './TypeGpu.vert';
 
 export function TypeGpu() {
-  const presentationFormat = navigator.gpu.getPreferredCanvasFormat();
-  const { device } = useDevice();
-  const clock = useClock();
+  const cbRef = useTypeGPU(
+    ({ canvas, context, device, gpu, presentationFormat, root }) => {
+      const { mainFragment, uniforms } = createFragmentShader(root);
 
-  const cbRef = (ref: CanvasRef | null) => {
-    const context = ref?.getContext('webgpu');
+      // First render
+      const pipeline = root['~unstable']
+        .withVertex(mainVertex, {})
+        .withFragment(mainFragment, { format: presentationFormat })
+        .createPipeline();
 
-    if (!device || !context) {
-      return;
+      // Animation loop
+      return ({ delta, time }) => {
+        uniforms.time.write(time);
+
+        pipeline
+          .withColorAttachment({
+            view: context.getCurrentTexture().createView(),
+            clearValue: [0, 0, 0, 0],
+            loadOp: 'clear',
+            storeOp: 'store',
+          })
+          .draw(3);
+      };
     }
-
-    const root = tgpu.initFromDevice({ device });
-
-    // const surface = context.canvas;
-    // const pxRatio = PixelRatio.get();
-    // surface.width = surface.width * pxRatio;
-    // surface.height = surface.height * pxRatio;
-
-    context.configure({
-      device: device,
-      format: presentationFormat,
-      alphaMode: 'premultiplied',
-    });
-
-    const { mainFragment, uniforms } = createFragmentShader(root);
-
-    // Rendering pipeline
-    const pipeline = root['~unstable']
-      .withVertex(mainVertex, {})
-      .withFragment(mainFragment, { format: presentationFormat })
-      .createPipeline();
-
-    let animationId: number;
-
-    // Animation loop
-    const animate = () => {
-      uniforms.time.write(clock.value / 1000);
-
-      pipeline
-        .withColorAttachment({
-          view: context.getCurrentTexture().createView(),
-          clearValue: [0, 0, 0, 0],
-          loadOp: 'clear',
-          storeOp: 'store',
-        })
-        .draw(3);
-
-      context.present();
-      animationId = requestAnimationFrame(animate);
-    };
-
-    context.present();
-    animationId = requestAnimationFrame(animate);
-
-    return () => {
-      cancelAnimationFrame(animationId);
-    };
-  };
+  );
 
   return <Canvas ref={cbRef} style={style.canvas} transparent />;
 }
---
+++ src/hooks/useTypeGPU.ts
@@ -0,0 +1,112 @@
+import { useClock } from '@shopify/react-native-skia';
+import { CanvasRef, RNCanvasContext, useDevice } from 'react-native-wgpu';
+import { tgpu, type TgpuRoot } from 'typegpu';
+
+interface RenderProps {
+  canvas: RNCanvasContext['canvas'];
+  context: RNCanvasContext;
+  device: GPUDevice;
+  gpu: GPU;
+  presentationFormat: GPUTextureFormat;
+  root: TgpuRoot;
+}
+
+type RenderArgs = {
+  /**
+   * Time elapsed since the last frame in seconds.
+   */
+  delta: number;
+  /**
+   * Total time elapsed in seconds.
+   */
+  time: number;
+};
+
+type RenderCallback = (args: RenderArgs) => void;
+
+type TgpuCallback = (props: RenderProps) => RenderCallback;
+type TgpuOptions = {
+  /**
+   * Delta time threshold in milliseconds at which frame normalization occurs.
+   * Set this to prevent physics sim explosions due to frame drops.
+   *
+   * @default 0.1 // 100ms
+   */
+  lagThreshold?: number;
+  /**
+   * Maximum delta step. Delta normalization value to use if `lagThreshold` is
+   * reached.
+   *
+   * @default 1 / 30 // ~30fps
+   */
+  maxDelta?: number;
+};
+
+const defaultOptions: Required<TgpuOptions> = {
+  lagThreshold: 0.1,
+  maxDelta: 1 / 30,
+};
+
+export function useTypeGPU(tgpuCallback: TgpuCallback, options?: TgpuOptions) {
+  const clock = useClock();
+  const { device } = useDevice();
+
+  const cbRef = (ref: CanvasRef | null) => {
+    const context = ref?.getContext('webgpu');
+
+    if (!device || !context) {
+      return;
+    }
+
+    const root = tgpu.initFromDevice({ device });
+    const presentationFormat = navigator.gpu.getPreferredCanvasFormat();
+
+    context.configure({
+      device: device,
+      format: presentationFormat,
+      alphaMode: 'premultiplied',
+    });
+
+    const sceneProps: RenderProps = {
+      canvas: context.canvas,
+      context,
+      device,
+      gpu: navigator.gpu,
+      presentationFormat,
+      root,
+    };
+
+    let renderCb = tgpuCallback(sceneProps);
+
+    let animationId: number;
+    let prev = 0;
+
+    const { lagThreshold, maxDelta } = Object.assign(
+      {},
+      options,
+      defaultOptions
+    );
+    const render = () => {
+      const ts = clock.value / 1000; // seconds
+      let dt = ts - prev;
+      prev = ts;
+
+      if (dt > lagThreshold) {
+        dt = Math.min(dt, maxDelta);
+      }
+
+      renderCb({ delta: dt, time: ts });
+      context.present();
+      animationId = requestAnimationFrame(render);
+    };
+
+    context.present();
+    animationId = requestAnimationFrame(render);
+
+    return () => {
+      cancelAnimationFrame(animationId);
+    };
+  };
+
+  return cbRef;
+}
