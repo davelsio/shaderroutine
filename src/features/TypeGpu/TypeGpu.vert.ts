47a8bdaa54ce5dc6cf7ae0bcb2bd6fb5651ca064
David Velasco <7377316+davelsio@users.noreply.github.com>
2026-01-17 11:42:21 +0100

    chore: remove superfluous tsconfig.json config fix: ignore noUncheckedIndexedAccess via @ts-expect-error

--- src/features/TypeGpu/TypeGpu.vert.ts
+++ src/features/TypeGpu/TypeGpu.vert.ts
@@ -4,11 +4,13 @@ import * as d from 'typegpu/data';
 export const mainVertex = tgpu['~unstable'].vertexFn({
   in: { vertexIndex: d.builtin.vertexIndex },
   out: { pos: d.builtin.position, uv: d.vec2f },
+  // @ts-expect-error noUncheckedIndexedAccess
 })(({ vertexIndex }) => {
   const pos = [d.vec2f(0, 0.8), d.vec2f(-0.8, -0.8), d.vec2f(0.8, -0.8)];
   const uv = [d.vec2f(0.5, 1), d.vec2f(0, 0), d.vec2f(1, 0)];
 
   return {
+    // @ts-expect-error noUncheckedIndexedAccess
     pos: d.vec4f(pos[vertexIndex], 0, 1),
     uv: uv[vertexIndex],
   };
